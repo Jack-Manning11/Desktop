2d44d5d8b0d7df0bd9bda3180e991826240fd58a
Jack-Manning11 <jama0209@colorado.edu>
2022-05-03 12:13:21 -0600

    js update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,25 @@
-var menuList = document.getElementById("menuList");
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+let menuList = document.getElementById("menuList");
 menuList.style.maxHeight = "0px";
 function togglemenu(){
   if(menuList.style.maxHeight == "0px")
