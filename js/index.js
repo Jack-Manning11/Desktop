dbf5d869b018ecd30188aedbbd879e2572c4ec09
Jack-Manning11 <jama0209@colorado.edu>
2022-05-03 12:25:58 -0600

    final touch?

--- js/index.js
+++ js/index.js
@@ -18,16 +18,3 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";
 }
-
-let menuList = document.getElementById("menuList");
-menuList.style.maxHeight = "0px";
-function togglemenu(){
-  if(menuList.style.maxHeight == "0px")
-  {
-    menuList.style.maxHeight = "130px";
-  }
-  else
-  {
-    menuList.style.maxHeight = "0px";
-  }
-}
