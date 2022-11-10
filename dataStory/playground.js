e2d2580e290748c3c2c815d93f39f9c5fbeeece7
Jack-Manning11 <jama0209@colorado.edu>
2022-11-10 10:57:54 -0700

    push

--- dataStory/playground.js
+++ dataStory/playground.js
@@ -3,6 +3,7 @@ const intro = document.querySelectorAll('.intro');
 const covers = document.querySelectorAll('.cover');
 const content = document.querySelectorAll('.content');
 const nextPage = document.querySelector('.btn-next');
+const backs = document.querySelectorAll('.backside')
 let count=1;
 
 book.addEventListener('click', () => {
@@ -17,9 +18,10 @@ nextPage.addEventListener('click', () => {
     let pageNum = content.length - count;
     content[pageNum].classList.add("turn");
     content[pageNum].classList.add("active");
-    console.log(pageNum);
+    backs[pageNum-1].classList.add("on");
     if(pageNum < content.length - 1){
         content[pageNum+1].classList.remove("active");
+        backs[pageNum].classList.remove("on");
     }
     count += 1;
 });
