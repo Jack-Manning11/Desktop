d877df04474b747455ba9cbee417602cdbc2f4a6
Jack-Manning11 <jama0209@colorado.edu>
2023-01-11 13:42:16 -0700

    first

--- test.js
+++ test.js
@@ -1,3 +1,57 @@
-addEventListener('click', ()=> {
-  randomCard(cards);
-});
+let players = [
+  {
+    name: "Alice",
+    FA: 0, 
+    attemps: 3
+  },
+  {
+    name: "Ben",
+    FA: 0, 
+    attemps: 3
+  },
+  {
+    name: "Charlie",
+    FA: 0, 
+    attemps: 3
+  },
+  {
+    name: "Dan",
+    FA: 0, 
+    attemps: 3
+  },
+  {
+    name: "Eli",
+    FA: 0, 
+    attemps: 3
+  },
+  {
+    name: "Frank",
+    FA: 0, 
+    attemps: 3
+  },
+  {
+    name: "Gary",
+    FA: 0, 
+    attemps: 3
+  },
+  {
+    name: "Hank",
+    FA: 0, 
+    attemps: 3
+  },
+  {
+    name: "Isaac",
+    FA: 0, 
+    attemps: 3
+  },
+  {
+    name: "Jack",
+    FA: 0, 
+    attemps: 3
+  },
+  {
+    name: "Kaladin",
+    FA: 0, 
+    attemps: 3
+  }
+]
