7492ab4cb0134310d9f404807ecafe58f53e5aad
Jack-Manning11 <jama0209@colorado.edu>
2023-07-25 21:32:33 -0600

    Revert "end of module 3"
    This reverts commit 89a279c872425ac838513008a75d7a47bbafa16c.

--- spotify-project/tutorial1/client/src/App.js
+++ spotify-project/tutorial1/client/src/App.js
@@ -19,20 +19,22 @@ const StyledLoginButton = styled.a`
 
 function App() {
 
-  const [token, setToken] = useState(null);
-  const [profile, setProfile] = useState(null);
-
   useEffect(() => {
-    setToken(accessToken);
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accessToken = urlParams.get('access_token');
+    const refreshToken = urlParams.get('refresh_token');
 
-    const fetchData = async () => {
-      const { data } = await getCurrentUserProfile();
-      setProfile(data);
+    console.log(accessToken);
+    console.log(refreshToken);
+
+    if (refreshToken) {
+      fetch(`/refresh_token?refresh_token=${refreshToken}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
     }
-
-    catchErrors(fetchData());
   }, []);
-  
   return (
     <div className="App">
       <header className="App-header">
--- spotify-project/tutorial1/index.js
+++ spotify-project/tutorial1/index.js
@@ -65,12 +65,11 @@ app.get('/callback', (req, res) => {
     })
     .then(response => {
       if (response.status === 200) {
-        const { access_token, refresh_token, expires_in } = response.data;
+        const { access_token, refresh_token } = response.data;
 
         const queryParams = querystring.stringify({
           access_token,
           refresh_token,
-          expires_in,
         });
 
         //redirect to react app
