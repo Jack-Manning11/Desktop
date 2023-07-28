771e31e762aec0cec866a69ae211f33443635f3b
Jack-Manning11 <jama0209@colorado.edu>
2023-07-27 19:46:12 -0600

    big push

--- spotify-project/tutorial1/client/src/pages/Profile.js
+++ spotify-project/tutorial1/client/src/pages/Profile.js
@@ -1,30 +1,55 @@
 import { useState, useEffect } from 'react';
 import { catchErrors } from '../utils';
-import { getCurrentUserProfile } from '../spotify';
+import { getCurrentUserProfile, getTopArtists } from '../spotify';
+import { StyledHeader } from '../styles';
+import { SectionWrapper, ArtistsGrid } from '../components';
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
+  const [topArtists, setTopArtists] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await getCurrentUserProfile();
-      setProfile(data);
+      const userProfile = await getCurrentUserProfile();
+      setProfile(userProfile.data);
+
+      const userTopArtists = await getTopArtists();
+      setTopArtists(userTopArtists.data);
     };
 
     catchErrors(fetchData());
   }, []);
+  console.log(topArtists);
 
   return (
     <>
       {profile && (
-        <div>
-          <h1>{profile.display_name}</h1>
-          <p>{profile.followers.total} Followers</p>
-          {profile.images.length && profile.images[1].url && (
-            <img src={profile.images[1].url} alt="Avatar"/>
-          )}
-        </div>
+        <>
+          <StyledHeader type="user">
+            <div className="header__inner">
+              {profile.images.length && profile.images[1].url && (
+                <img className="header__img" src={profile.images[1].url} alt="Avatar"/>
+              )}
+              <div>
+                <div className="header__overline">Profile</div>
+                <h1 className="header__name">{profile.display_name}</h1>
+                <p className="header__meta">
+                  <span>
+                    {profile.followers.total} Follower{profile.followers.total !== 1 ? 's' : ''}
+                  </span>
+                </p>
+              </div>
+            </div>
+          </StyledHeader>
+        </>
       )}
+      {topArtists && (
+            <main>
+              <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
+                <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
+              </SectionWrapper>
+            </main>
+          )}
     </>
   )
 };
