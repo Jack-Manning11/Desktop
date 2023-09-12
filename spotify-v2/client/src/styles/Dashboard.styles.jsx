f2f0e479014cdf2467cddae7a72b9f2a0f748d87
Jack-Manning11 <jama0209@colorado.edu>
2023-09-12 09:44:57 -0600

    tweaking

--- spotify-v2/client/src/styles/Dashboard.styles.jsx
+++ spotify-v2/client/src/styles/Dashboard.styles.jsx
@@ -6,25 +6,6 @@ export const DashBoardContainer = styled.div`
     width: 100%;
 `
 
-export const SearchInput = styled.input`
-    width: 100%;
-    height: 55px;
-    padding: 10px;
-    border-radius: 5px;
-    font-size: 1.5rem;
-
-    &::placeholder {
-        font-size: 1.5rem;
-    }
-`
-
-export const ResultsContainer = styled.div`
-    flex-grow: 1;
-    margin: 3rem 0;
-    overflow-y: auto;
-    overflow-x: auto;
-`
-
 export const PlayerContainer = styled.div`
     width: 100%;
     position: fixed;
